4d8f253a4c7772f799d0586c2d8e1d5fa59e5fd2
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2018-05-23 00:48:58 +0900

    style: indentをtabに統一

--- chrome-extension/js/Schedule.js
+++ chrome-extension/js/Schedule.js
@@ -1,65 +1,65 @@
 
 class Schedule {
-    /**
-     * 
-     * @param {object} schedule - original schedule data
-     */
-    constructor(schedule) {
-        this._originalData = schedule;
-    }
+	/**
+	 * 
+	 * @param {object} schedule - original schedule data
+	 */
+	constructor(schedule) {
+		this._originalData = schedule;
+	}
 
-    /**
-     * @param {object} arg
-     * @param {object} arg._originalData
-     * @returns {Schedule} aa
-     */
-    static parse(arg) {
-        return new this(arg._originalData);
-    }
+	/**
+	 * @param {object} arg
+	 * @param {object} arg._originalData
+	 * @returns {Schedule} aa
+	 */
+	static parse(arg) {
+		return new this(arg._originalData);
+	}
 
-    /**
-     * @returns {boolean}
-     */
-    get isValid() {
-        return !!this._originalData.record;
-    }
+	/**
+	 * @returns {boolean}
+	 */
+	get isValid() {
+		return !!this._originalData.record;
+	}
 
-    /**
-     * @returns {string}
-     */
-    get id() {
-        if (this.isValid) {
-            const resid = this._originalData.record.resid;
-            const target = this._originalData.record.target;        
-            return `${resid}:${target}`;
-        }
-    }
+	/**
+	 * @returns {string}
+	 */
+	get id() {
+		if (this.isValid) {
+			const resid = this._originalData.record.resid;
+			const target = this._originalData.record.target;		
+			return `${resid}:${target}`;
+		}
+	}
 
-    /**
-     * @returns {string}
-     */
-    get resid() {
-        return this._originalData.record.resid;
-    }
+	/**
+	 * @returns {string}
+	 */
+	get resid() {
+		return this._originalData.record.resid;
+	}
 
-    /**
-     * @returns {string} YYYY-MM-DD
-     */
-    get targetDate() {
-        return this._originalData.record.target;
-    }
+	/**
+	 * @returns {string} YYYY-MM-DD
+	 */
+	get targetDate() {
+		return this._originalData.record.target;
+	}
 
-    /**
-     * @returns {string}
-     */
-    get title() {
-        return this._originalData.title;
-    }
+	/**
+	 * @returns {string}
+	 */
+	get title() {
+		return this._originalData.title;
+	}
 
-    /**
-     * @returns {string}
-     */
-    get time() {
-        return this._originalData.time;
-    }
+	/**
+	 * @returns {string}
+	 */
+	get time() {
+		return this._originalData.time;
+	}
 };
--- chrome-extension/schedule/main.js
+++ chrome-extension/schedule/main.js
@@ -5,10 +5,10 @@
  * @param {Date} date 
  */
 const generateCalendarDateFormat = date => {
-    const yyyy = date.getFullYear();
-    const mm = String(date.getMonth() + 1).padStart(2, '0');
-    const dd = String(date.getDate()).padStart(2, '0');
-    return `${yyyy}-${mm}-${dd}`;
+	const yyyy = date.getFullYear();
+	const mm = String(date.getMonth() + 1).padStart(2, '0');
+	const dd = String(date.getDate()).padStart(2, '0');
+	return `${yyyy}-${mm}-${dd}`;
 };
 
 /**
@@ -16,12 +16,12 @@ const generateCalendarDateFormat = date => {
  * @param {Date} date 
  */
 const formatDate = date => {
-    return date.toLocaleString('ja-JP', {
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-        weekday: 'short',
-    });
+	return date.toLocaleString('ja-JP', {
+		year: 'numeric',
+		month: '2-digit',
+		day: '2-digit',
+		weekday: 'short',
+	});
 };
 
 /**
@@ -29,110 +29,110 @@ const formatDate = date => {
  * @param {Date} date 
  */
 const formatDateTime = date => {
-    return date.toLocaleString('ja-JP', {
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-        weekday: 'short',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-    });
+	return date.toLocaleString('ja-JP', {
+		year: 'numeric',
+		month: '2-digit',
+		day: '2-digit',
+		weekday: 'short',
+		hour: '2-digit',
+		minute: '2-digit',
+		second: '2-digit',
+	});
 };
 
 const targetUrlSettingStorageKey = 'target_url';
 chrome.storage.local.get({
-    [targetUrlSettingStorageKey]: '',
+	[targetUrlSettingStorageKey]: '',
 }, items => {
-    const targetUrl = items[targetUrlSettingStorageKey];
+	const targetUrl = items[targetUrlSettingStorageKey];
 
-    if (targetUrl) {
-        chrome.storage.local.get('scheduleCache', ({scheduleCache}) => {
-            if (scheduleCache) {
-                const scheduleById = Object.assign(...Object.entries(scheduleCache.scheduleById).map(([id, obj]) => {
-                    return {
-                        [id]: Schedule.parse(obj),
-                    };
-                }));
+	if (targetUrl) {
+		chrome.storage.local.get('scheduleCache', ({scheduleCache}) => {
+			if (scheduleCache) {
+				const scheduleById = Object.assign(...Object.entries(scheduleCache.scheduleById).map(([id, obj]) => {
+					return {
+						[id]: Schedule.parse(obj),
+					};
+				}));
 
-                displayEvents(scheduleCache.dailySchedules, scheduleById, targetUrl, scheduleCache.lastModified);
-            }
-        });
+				displayEvents(scheduleCache.dailySchedules, scheduleById, targetUrl, scheduleCache.lastModified);
+			}
+		});
 
-        document.getElementById('refresh').addEventListener('click', ({target}) => {
-            target.disabled = true;
-            document.getElementById('message').innerText = '';
+		document.getElementById('refresh').addEventListener('click', ({target}) => {
+			target.disabled = true;
+			document.getElementById('message').innerText = '';
 
-            fetchSchedule().then(scheduleCache => {
-                displayEvents(scheduleCache.dailySchedules, scheduleCache.scheduleById, targetUrl, scheduleCache.lastModified);
-            }).catch(err => {
-                console.error(err);
-                document.getElementById('message').innerText = `取得に失敗しました（${err}）`;
-            }).then(() => {
-                target.disabled = false;
-            });
-        });
-    
-        const fetchSchedule = () => {
-            const params = new URLSearchParams();
-            params.set('exa', 'calendar');
-            params.set('viewId', 'schedule-list/to-json');
-            params.set('start', generateCalendarDateFormat(new Date()));
-            params.toString();
+			fetchSchedule().then(scheduleCache => {
+				displayEvents(scheduleCache.dailySchedules, scheduleCache.scheduleById, targetUrl, scheduleCache.lastModified);
+			}).catch(err => {
+				console.error(err);
+				document.getElementById('message').innerText = `取得に失敗しました（${err}）`;
+			}).then(() => {
+				target.disabled = false;
+			});
+		});
+	
+		const fetchSchedule = () => {
+			const params = new URLSearchParams();
+			params.set('exa', 'calendar');
+			params.set('viewId', 'schedule-list/to-json');
+			params.set('start', generateCalendarDateFormat(new Date()));
+			params.toString();
 
-            return fetch(`${targetUrl}aqua/0/schedule/view?${params.toString()}`, {
-                credentials: 'include',
-            }).then(res => {
-                const contentType = res.headers.get('content-type');
-                if (contentType.startsWith('application/json')) {
-                    return res.json(); 
-                }
-                // レスポンスがJSONでない
-                throw 'おそらくログインしてない';
-            }).then(json => {
-                const scheduleById = {};
-                for (const event of Object.values(json.events)) {
-                    for (const s of event.schedules) {
-                        const schedule = new Schedule(s);
-                        if (schedule.isValid) {
-                            scheduleById[schedule.id] = schedule;
-                        }
-                    }
-                }
+			return fetch(`${targetUrl}aqua/0/schedule/view?${params.toString()}`, {
+				credentials: 'include',
+			}).then(res => {
+				const contentType = res.headers.get('content-type');
+				if (contentType.startsWith('application/json')) {
+					return res.json(); 
+				}
+				// レスポンスがJSONでない
+				throw 'おそらくログインしてない';
+			}).then(json => {
+				const scheduleById = {};
+				for (const event of Object.values(json.events)) {
+					for (const s of event.schedules) {
+						const schedule = new Schedule(s);
+						if (schedule.isValid) {
+							scheduleById[schedule.id] = schedule;
+						}
+					}
+				}
 
-                const dailySchedules = Object.entries(json.events).sort(([date1], [date2]) => {
-                    if (date1 > date2) return 1;
-                    if (date1 < date2) return -1;
-                    return 0;
-                }).map(([date, {schedules}]) => {
-                    const scheduleIds = schedules.map(s => new Schedule(s)).filter(schedule => schedule.isValid).map(schedule => schedule.id);
-                    return {
-                        date,
-                        scheduleIds,
-                    };
-                });
+				const dailySchedules = Object.entries(json.events).sort(([date1], [date2]) => {
+					if (date1 > date2) return 1;
+					if (date1 < date2) return -1;
+					return 0;
+				}).map(([date, {schedules}]) => {
+					const scheduleIds = schedules.map(s => new Schedule(s)).filter(schedule => schedule.isValid).map(schedule => schedule.id);
+					return {
+						date,
+						scheduleIds,
+					};
+				});
 
-                return {
-                    dailySchedules,
-                    scheduleById,
-                };
-            }).then(({dailySchedules, scheduleById}) => {
-                const scheduleCache = {
-                    dailySchedules,
-                    scheduleById,
-                    lastModified: Date.now(),
-                };
+				return {
+					dailySchedules,
+					scheduleById,
+				};
+			}).then(({dailySchedules, scheduleById}) => {
+				const scheduleCache = {
+					dailySchedules,
+					scheduleById,
+					lastModified: Date.now(),
+				};
 
-                chrome.storage.local.set({
-                    scheduleCache,
-                });
+				chrome.storage.local.set({
+					scheduleCache,
+				});
 
-                scheduleAlarms(dailySchedules, scheduleById);
+				scheduleAlarms(dailySchedules, scheduleById);
 
-                return scheduleCache;
-            });
-        };
-    }
+				return scheduleCache;
+			});
+		};
+	}
 });
 
 /**
@@ -143,34 +143,34 @@ chrome.storage.local.get({
  * @param {Object.<string, Schedule>} scheduleById 
  */
 const scheduleAlarms = (dailySchedules, scheduleById) => {
-    const MS_5_MINUTES = 1000 * 60 * 5;
+	const MS_5_MINUTES = 1000 * 60 * 5;
 
-    chrome.alarms.clearAll(() => {
-        dailySchedules.forEach(({date, scheduleIds}) => {
-            scheduleIds.forEach(scheduleId => {
-                const schedule = scheduleById[scheduleId];
+	chrome.alarms.clearAll(() => {
+		dailySchedules.forEach(({date, scheduleIds}) => {
+			scheduleIds.forEach(scheduleId => {
+				const schedule = scheduleById[scheduleId];
 
-                if (schedule.time.match(/^(\d+:\d+) -/)) {
-                    const startTime = RegExp.$1;
-                    const eventStartDate = new Date(`${date} ${startTime}`);
+				if (schedule.time.match(/^(\d+:\d+) -/)) {
+					const startTime = RegExp.$1;
+					const eventStartDate = new Date(`${date} ${startTime}`);
 
-                    const when = eventStartDate.getTime() - MS_5_MINUTES;
+					const when = eventStartDate.getTime() - MS_5_MINUTES;
 
-                    if (when > Date.now()) {
-                        const alarmParams = new URLSearchParams();
-                        alarmParams.set('type', 'schedule');
-                        alarmParams.set('date', date);
-                        alarmParams.set('scheduleId', scheduleId);
-                        alarmParams.set('title', schedule.title);
+					if (when > Date.now()) {
+						const alarmParams = new URLSearchParams();
+						alarmParams.set('type', 'schedule');
+						alarmParams.set('date', date);
+						alarmParams.set('scheduleId', scheduleId);
+						alarmParams.set('title', schedule.title);
 
-                        chrome.alarms.create(alarmParams.toString(), {
-                            when,
-                        });
-                    }
-                }
-            });
-        });
-    });
+						chrome.alarms.create(alarmParams.toString(), {
+							when,
+						});
+					}
+				}
+			});
+		});
+	});
 };
 
 
@@ -184,39 +184,39 @@ const scheduleAlarms = (dailySchedules, scheduleById) => {
  * @param {number} lastModified
  */
 const displayEvents = (dailySchedules, scheduleById, targetUrl, lastModified) => {
-    const lastModifiedElement = document.getElementById('lastModified');
-    lastModifiedElement.innerText = `取得日: ${formatDateTime(new Date(lastModified))}`;
+	const lastModifiedElement = document.getElementById('lastModified');
+	lastModifiedElement.innerText = `取得日: ${formatDateTime(new Date(lastModified))}`;
 
-    const container = document.getElementById('schedule_container');
-    container.innerText = '';
+	const container = document.getElementById('schedule_container');
+	container.innerText = '';
 
-    dailySchedules.forEach(({date, scheduleIds}) => {
-        const h1 = document.createElement('h1');
-        h1.append(formatDate(new Date(date)));
-        container.append(h1);
-        scheduleIds.forEach(scheduleId => {
-            const schedule = scheduleById[scheduleId];
-            const url = `${targetUrl}aqua/${schedule.resid}/view?target=${date}`;
-            
-            const p = document.createElement('p');
-            p.append(schedule.time);
-            p.append(': ');
-            const link = document.createElement('a');
-            link.append(schedule.title);
-            link.href = url;
-            link.target = '_blank';
-            p.append(link);
-            container.append(p);
+	dailySchedules.forEach(({date, scheduleIds}) => {
+		const h1 = document.createElement('h1');
+		h1.append(formatDate(new Date(date)));
+		container.append(h1);
+		scheduleIds.forEach(scheduleId => {
+			const schedule = scheduleById[scheduleId];
+			const url = `${targetUrl}aqua/${schedule.resid}/view?target=${date}`;
+			
+			const p = document.createElement('p');
+			p.append(schedule.time);
+			p.append(': ');
+			const link = document.createElement('a');
+			link.append(schedule.title);
+			link.href = url;
+			link.target = '_blank';
+			p.append(link);
+			container.append(p);
 
-            link.addEventListener('click', evt => {
-                evt.preventDefault();
-                chrome.windows.create({
-                    url,
-                    type: chrome.windows.CreateType.POPUP,
-                    width: 1200,
-                    height: 800,
-                });
-            });
-        });
-    });
+			link.addEventListener('click', evt => {
+				evt.preventDefault();
+				chrome.windows.create({
+					url,
+					type: chrome.windows.CreateType.POPUP,
+					width: 1200,
+					height: 800,
+				});
+			});
+		});
+	});
 };
