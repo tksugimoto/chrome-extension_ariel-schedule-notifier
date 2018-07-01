c9f521df1d6dd53606ed1dec162525a22eaee2ee
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2018-07-02 00:55:41 +0900

    Revert "Merge branch 'refactoring/scope'"
    This reverts commit 29dbb659dbb977526cd6fdd772f2a1a9882314e8, reversing
    changes made to bf4afa330597d516fd94299b8e02dde90a88ecb7.

--- chrome-extension/js/displayEvents.js
+++ chrome-extension/js/displayEvents.js
@@ -1,84 +1,83 @@
-{
-	/**
-	 *
-	 * @param {Date} date
-	 */
-	const formatDate = date => {
-		return date.toLocaleString('ja-JP', {
-			year: 'numeric',
-			month: '2-digit',
-			day: '2-digit',
-			weekday: 'short',
-		});
-	};
 
-	/**
-	 *
-	 * @param {Date} date
-	 */
-	const formatDateTime = date => {
-		return date.toLocaleString('ja-JP', {
-			year: 'numeric',
-			month: '2-digit',
-			day: '2-digit',
-			weekday: 'short',
-			hour: '2-digit',
-			minute: '2-digit',
-			second: '2-digit',
-		});
-	};
+/**
+ *
+ * @param {Date} date
+ */
+const formatDate = date => {
+	return date.toLocaleString('ja-JP', {
+		year: 'numeric',
+		month: '2-digit',
+		day: '2-digit',
+		weekday: 'short',
+	});
+};
 
-	/**
-	 *
-	 * @param {object[]} dailySchedules
-	 * @param {string} dailySchedules[].date
-	 * @param {string[]} dailySchedules[].scheduleIds
-	 * @param {Object.<string, Schedule>} scheduleById
-	 * @param {string} targetUrl
-	 * @param {number} lastModified
-	 */
-	window.displayEvents = (dailySchedules, scheduleById, targetUrl, lastModified, option = {}) => {
-		const lastModifiedElement = document.getElementById(option.lastModifiedElementId || 'lastModified');
-		lastModifiedElement.innerText = `取得日: ${formatDateTime(new Date(lastModified))}`;
+/**
+ *
+ * @param {Date} date
+ */
+const formatDateTime = date => {
+	return date.toLocaleString('ja-JP', {
+		year: 'numeric',
+		month: '2-digit',
+		day: '2-digit',
+		weekday: 'short',
+		hour: '2-digit',
+		minute: '2-digit',
+		second: '2-digit',
+	});
+};
 
-		const container = document.getElementById(option.containerElementId || 'schedule_container');
-		container.innerText = '';
+/**
+ *
+ * @param {object[]} dailySchedules
+ * @param {string} dailySchedules[].date
+ * @param {string[]} dailySchedules[].scheduleIds
+ * @param {Object.<string, Schedule>} scheduleById
+ * @param {string} targetUrl
+ * @param {number} lastModified
+ */
+window.displayEvents = (dailySchedules, scheduleById, targetUrl, lastModified, option = {}) => {
+	const lastModifiedElement = document.getElementById(option.lastModifiedElementId || 'lastModified');
+	lastModifiedElement.innerText = `取得日: ${formatDateTime(new Date(lastModified))}`;
 
-		dailySchedules.forEach(({date, scheduleIds}) => {
-			const h1 = document.createElement('h1');
-			h1.append(formatDate(new Date(date)));
-			container.append(h1);
-			scheduleIds.forEach(scheduleId => {
-				const schedule = scheduleById[scheduleId];
-				const url = `${targetUrl}aqua/${schedule.resid}/view?target=${schedule.targetDate}`;
+	const container = document.getElementById(option.containerElementId || 'schedule_container');
+	container.innerText = '';
 
-				const p = document.createElement('p');
-				p.append(schedule.time);
-				p.append(': ');
-				const link = document.createElement('a');
-				link.append(schedule.title);
-				link.href = url;
-				link.target = '_blank';
-				p.append(link);
+	dailySchedules.forEach(({date, scheduleIds}) => {
+		const h1 = document.createElement('h1');
+		h1.append(formatDate(new Date(date)));
+		container.append(h1);
+		scheduleIds.forEach(scheduleId => {
+			const schedule = scheduleById[scheduleId];
+			const url = `${targetUrl}aqua/${schedule.resid}/view?target=${schedule.targetDate}`;
 
-				const facility = schedule.findFromOptions('施設');
-				if (facility) {
-					p.append(document.createElement('br'));
-					p.append(`施設: ${facility}`);
-				}
+			const p = document.createElement('p');
+			p.append(schedule.time);
+			p.append(': ');
+			const link = document.createElement('a');
+			link.append(schedule.title);
+			link.href = url;
+			link.target = '_blank';
+			p.append(link);
 
-				container.append(p);
+			const facility = schedule.findFromOptions('施設');
+			if (facility) {
+				p.append(document.createElement('br'));
+				p.append(`施設: ${facility}`);
+			}
 
-				link.addEventListener('click', evt => {
-					evt.preventDefault();
-					chrome.windows.create({
-						url,
-						type: chrome.windows.CreateType.POPUP,
-						width: 1200,
-						height: 800,
-					});
+			container.append(p);
+
+			link.addEventListener('click', evt => {
+				evt.preventDefault();
+				chrome.windows.create({
+					url,
+					type: chrome.windows.CreateType.POPUP,
+					width: 1200,
+					height: 800,
 				});
 			});
 		});
-	};
-}
+	});
+};
--- chrome-extension/js/fetchSchedule.js
+++ chrome-extension/js/fetchSchedule.js
@@ -1,123 +1,122 @@
 /* global Schedule AlarmUtil */
-{
-	/**
-	 *
-	 * @param {Date} date
-	 */
-	const generateCalendarDateFormat = date => {
-		const yyyy = date.getFullYear();
-		const mm = String(date.getMonth() + 1).padStart(2, '0');
-		const dd = String(date.getDate()).padStart(2, '0');
-		return `${yyyy}-${mm}-${dd}`;
-	};
 
-	/**
-	 *
-	 * @param {Date} refreshDate
-	 */
-	const formatRefreshDateTime = refreshDate => {
-		return refreshDate.toLocaleString('ja-JP', {
-			month: '2-digit',
-			day: '2-digit',
-			weekday: 'short',
-			hour: '2-digit',
-			minute: '2-digit',
-			second: '2-digit',
-		});
-	};
+/**
+ *
+ * @param {Date} date
+ */
+const generateCalendarDateFormat = date => {
+	const yyyy = date.getFullYear();
+	const mm = String(date.getMonth() + 1).padStart(2, '0');
+	const dd = String(date.getDate()).padStart(2, '0');
+	return `${yyyy}-${mm}-${dd}`;
+};
 
-	const updateStatus = ({
-		refreshSucceeded,
-		refreshDate,
-	}) => {
-		const status = refreshSucceeded ? '更新成功' : '更新失敗';
-		const badgeText = refreshSucceeded ? '' : 'X';
-		const badgeBackgroundColor = refreshSucceeded ? [0, 0, 0, 0] : 'red';
+/**
+ *
+ * @param {Date} refreshDate
+ */
+const formatRefreshDateTime = refreshDate => {
+	return refreshDate.toLocaleString('ja-JP', {
+		month: '2-digit',
+		day: '2-digit',
+		weekday: 'short',
+		hour: '2-digit',
+		minute: '2-digit',
+		second: '2-digit',
+	});
+};
 
-		const extensionName = chrome.runtime.getManifest().name;
-		chrome.browserAction.setTitle({
-			title: `${extensionName}\n${status}\n${formatRefreshDateTime(refreshDate)}`,
-		});
-		chrome.browserAction.setBadgeText({
-			text: badgeText,
-		});
-		chrome.browserAction.setBadgeBackgroundColor({
-			color: badgeBackgroundColor,
-		});
-	};
+const updateStatus = ({
+	refreshSucceeded,
+	refreshDate,
+}) => {
+	const status = refreshSucceeded ? '更新成功' : '更新失敗';
+	const badgeText = refreshSucceeded ? '' : 'X';
+	const badgeBackgroundColor = refreshSucceeded ? [0, 0, 0, 0] : 'red';
 
-	/**
-	 * @param {string} targetUrl
-	 */
-	window.fetchSchedule = (targetUrl) => {
-		const params = new URLSearchParams();
-		params.set('exa', 'calendar');
-		params.set('viewId', 'schedule-list/to-json');
-		params.set('start', generateCalendarDateFormat(new Date()));
-		params.toString();
+	const extensionName = chrome.runtime.getManifest().name;
+	chrome.browserAction.setTitle({
+		title: `${extensionName}\n${status}\n${formatRefreshDateTime(refreshDate)}`,
+	});
+	chrome.browserAction.setBadgeText({
+		text: badgeText,
+	});
+	chrome.browserAction.setBadgeBackgroundColor({
+		color: badgeBackgroundColor,
+	});
+};
 
-		return fetch(`${targetUrl}aqua/0/schedule/view?${params.toString()}`, {
-			credentials: 'include',
-		}).then(res => {
-			const contentType = res.headers.get('content-type');
-			if (contentType.startsWith('application/json')) {
-				return res.json();
-			}
-			// レスポンスがJSONでない
-			throw 'おそらくログインしてない';
-		}).then(json => {
-			const scheduleById = {};
-			for (const [date, event] of Object.entries(json.events)) {
-				for (const s of event.schedules) {
-					const schedule = new Schedule(s, date);
-					if (schedule.isValid) {
-						scheduleById[schedule.id] = schedule;
-					}
+/**
+ * @param {string} targetUrl
+ */
+window.fetchSchedule = (targetUrl) => {
+	const params = new URLSearchParams();
+	params.set('exa', 'calendar');
+	params.set('viewId', 'schedule-list/to-json');
+	params.set('start', generateCalendarDateFormat(new Date()));
+	params.toString();
+
+	return fetch(`${targetUrl}aqua/0/schedule/view?${params.toString()}`, {
+		credentials: 'include',
+	}).then(res => {
+		const contentType = res.headers.get('content-type');
+		if (contentType.startsWith('application/json')) {
+			return res.json();
+		}
+		// レスポンスがJSONでない
+		throw 'おそらくログインしてない';
+	}).then(json => {
+		const scheduleById = {};
+		for (const [date, event] of Object.entries(json.events)) {
+			for (const s of event.schedules) {
+				const schedule = new Schedule(s, date);
+				if (schedule.isValid) {
+					scheduleById[schedule.id] = schedule;
 				}
 			}
+		}
 
-			const dailySchedules = Object.entries(json.events).sort(([date1], [date2]) => {
-				if (date1 > date2) return 1;
-				if (date1 < date2) return -1;
-				return 0;
-			}).map(([date, {schedules}]) => {
-				const scheduleIds = schedules.map(s => new Schedule(s, date)).filter(schedule => schedule.isValid).map(schedule => schedule.id);
-				return {
-					date,
-					scheduleIds,
-				};
-			});
-
+		const dailySchedules = Object.entries(json.events).sort(([date1], [date2]) => {
+			if (date1 > date2) return 1;
+			if (date1 < date2) return -1;
+			return 0;
+		}).map(([date, {schedules}]) => {
+			const scheduleIds = schedules.map(s => new Schedule(s, date)).filter(schedule => schedule.isValid).map(schedule => schedule.id);
 			return {
-				dailySchedules,
-				scheduleById,
+				date,
+				scheduleIds,
 			};
-		}).then(({dailySchedules, scheduleById}) => {
-			const scheduleCache = {
-				dailySchedules,
-				scheduleById,
-				lastModified: Date.now(),
-			};
-
-			chrome.storage.local.set({
-				scheduleCache,
-			});
-
-			AlarmUtil.refreshScheduleNotificationAlarms(dailySchedules, scheduleById);
-
-			return scheduleCache;
-		}).then(scheduleCache => {
-			updateStatus({
-				refreshSucceeded: true,
-				refreshDate: new Date(scheduleCache.lastModified),
-			});
-			return scheduleCache;
-		}).catch(err => {
-			updateStatus({
-				refreshSucceeded: false,
-				refreshDate: new Date,
-			});
-			throw err;
 		});
-	};
-}
+
+		return {
+			dailySchedules,
+			scheduleById,
+		};
+	}).then(({dailySchedules, scheduleById}) => {
+		const scheduleCache = {
+			dailySchedules,
+			scheduleById,
+			lastModified: Date.now(),
+		};
+
+		chrome.storage.local.set({
+			scheduleCache,
+		});
+
+		AlarmUtil.refreshScheduleNotificationAlarms(dailySchedules, scheduleById);
+
+		return scheduleCache;
+	}).then(scheduleCache => {
+		updateStatus({
+			refreshSucceeded: true,
+			refreshDate: new Date(scheduleCache.lastModified),
+		});
+		return scheduleCache;
+	}).catch(err => {
+		updateStatus({
+			refreshSucceeded: false,
+			refreshDate: new Date,
+		});
+		throw err;
+	});
+};
